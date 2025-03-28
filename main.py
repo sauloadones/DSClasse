69b1bc3ac977833c3d4cc9f4272918c2ee09d971
MatmJr <matmj@cin.ufpe.br>
2025-03-28 15:54:41 -0300

    Aula 2

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import requests
 import pandas as pd
+from src.extractTransform import requestApiBcb
+from src.load import salvarCsv
 
-def etlBcB(data):
-    """
-    Função para extrair os dados da API do Banco Central.
-    """
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json" 
-    req = requests.get(url) 
-    print("Status Code:", req.status_code)
-    data = req.json() 
+dadosBcb = requestApiBcb('20191')
+salvarCsv(dadosBcb, "etlBCB/src/datasets/meiosPagamentosTri.csv", ";",".")
 
-    df = pd.json_normalize(data["value"])
-    # print(df)
-    return df
